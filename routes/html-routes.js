f9c7950c7cf1b04c0070c5fcbe0ce9f32d0e0296
John D. Riepe <JohnD.Riepe@Danielles-MacBook-Pro.local>
2016-07-22 19:37:39 -0400

    trying to make chartist work

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -125,8 +125,14 @@ module.exports = function(app){
 
 	app.get('/chart1', function(req, res){
 		if (req.isAuthenticated()) {
-			res.render('chart1', {
-				
+			ormdb.selectAll(req.user.userId, function(result){
+		           
+		            console.log('app.get: ')
+		            console.log(result[0].invName)
+		            
+		            res.render('chart1', {
+		            	chartvar: result
+		            });
 			});
 		}
 		else {
